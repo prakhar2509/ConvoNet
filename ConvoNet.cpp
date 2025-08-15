8934086407c2219ca7cad33ee65265140a13553e
prakhar2509 <prakharsingh2509@gmail.com>
2025-08-15 17:20:08 +0530

    Intial version

---
+++ ConvoNet.cpp
@@ -0,0 +1,12 @@
+﻿// ConvoNet.cpp : Defines the entry point for the application.
+//
+
+#include "ConvoNet.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
